0350996a22df54f7bbadaa6dda17fd92d52bdd97
Julian <michaeljulian50@gmail.com>
2022-01-23 23:17:40 -0500

    Code base64

---
+++ Platzi-punks/contracts/PlatziPunks.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
+
+contract PlatziPunks is ERC721 , ERC721Enumerable{
+
+    using Counters for Counters.Counter;
+
+    Counters.Counters private _idCounter;
+    uint256 public masSupply;
+
+    constructor (uint256 _maxSupply) ERC721('PlatziPunks', 'PLPKS'){
+        maxSupply = _maxSupply
+    }
+
+    function mint () public{
+        uint256 current = _idCounter.current();
+
+         require (current < maxSupply , "No platzy left")
+         _safeMint (msg.sender , current);
+         _idCounter.increment()
+        
+    }
+
+     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+       function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "PlatziPunks #',
+                tokenId,
+                '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                "// TODO: Calculate image URL",
+                '"}'
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
+
+} 
